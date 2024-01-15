e4171a37a76eb22c59c428d02abeaec127ca7590
Gerardoruiz1 <itzsgera@gmail.com>
2024-01-15 12:13:54 -0400

    Guard added to summary and country

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { canActivate,redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+import { canActivate,redirectUnauthorizedTo, } from '@angular/fire/auth-guard';
+import { AngularFireAuthGuard } from '@angular/fire/compat/auth-guard';
+
+const redirectUnauthorizedToRegister = () => redirectUnauthorizedTo(['/register']);
 
 const routes: Routes = [
 { path: '',redirectTo:'/summary',pathMatch:'full'},  
 { path: 'register',component: RegisterComponent },
 { path: 'login', component: LoginComponent },
-{ path: 'summary',loadChildren: () => import('./summary/summary.module').then(m=>m.SummaryModule), ...canActivate(()=>redirectUnauthorizedTo(['/register']))},// the intention for can activate here is to not give acces to any page unles they are registered but it isnt working
-{ path: 'viewers',loadChildren: () => import('./viewers/live.module').then(m=>m.ViewersModule) },
-{ path: 'country',loadChildren: () => import('./country/country.module').then(m=>m.CountryModule) },
+{ path: 'summary',loadChildren: () => import('./summary/summary.module').then(m=>m.SummaryModule),canActivate: [AngularFireAuthGuard],data: { authGuardPipe: redirectUnauthorizedToRegister }},
+{ path: 'viewers',loadChildren: () => import('./viewers/live.module').then(m=>m.ViewersModule) ,canActivate: [AngularFireAuthGuard],data: { authGuardPipe: redirectUnauthorizedToRegister }},
+{ path: 'country',loadChildren: () => import('./country/country.module').then(m=>m.CountryModule),canActivate: [AngularFireAuthGuard],data: { authGuardPipe: redirectUnauthorizedToRegister }},
 
 
 ];
