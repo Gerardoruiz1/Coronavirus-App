88ced02daeb7b871b9ca8491dd7ce1e45c94c117
Gerardoruiz1 <itzsgera@gmail.com>
2024-01-16 07:12:57 -0400

    logout functionality added

--- src/app/shared/components/topbar/topbar.component.ts
+++ src/app/shared/components/topbar/topbar.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { AuthService } from 'src/app/auth/services/auth.service';
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
   styleUrls: ['./topbar.component.css']
 })
 export class TopbarComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   navigate(path: string) {
     this.router.navigate([path]);
+  }
+  logout() {
+    this.authService.logout().then(() => {
+      this.router.navigate(['/login']); // Redirect to login page after logout
+    }).catch(error => {
+      console.error('Logout error:', error);
+    });
   }
 }
