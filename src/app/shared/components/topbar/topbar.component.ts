665e926663f0f97b4c7a275fb412426a1729a322
Gerardoruiz1 <itzsgera@gmail.com>
2024-01-16 07:24:12 -0400

    login button funtionality added

--- src/app/shared/components/topbar/topbar.component.ts
+++ src/app/shared/components/topbar/topbar.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/services/auth.service';
+import { Observable, map } from 'rxjs';
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
   styleUrls: ['./topbar.component.css']
 })
 export class TopbarComponent {
-  constructor(private router: Router, private authService: AuthService) {}
+  isLoggedIn$: Observable<boolean>;
+
+  constructor(private router: Router, private authService: AuthService) {  this.isLoggedIn$ = this.authService.getUser().pipe(
+    map(user => !!user)
+  );}
 
   navigate(path: string) {
     this.router.navigate([path]);
